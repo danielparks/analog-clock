c80f7c46b8119cf8805bb222312e51239f83d4da
Daniel Parks <dp+git@oxidized.org>
2025-02-21 17:14:24 -0800

    Convert to stopwatch; progress with clicks.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,21 @@
-var time = {},
-    hours = 0,
-    minutes = 0,
-    seconds = 0;
-
-function init() {
-    time = new Date();
-    hours = time.getHours();
-    minutes = time.getMinutes();
-    seconds = time.getSeconds();
-
+window.addEventListener('DOMContentLoaded', () => {
     var tickSpan = '<span></span>'.repeat(60);
     document.getElementsByClassName('ticks')[0].innerHTML = tickSpan;
 
-    document.getElementById("second").className = "second second-" + seconds;
-    document.getElementById("minute").className = "minute minute-" + minutes;
-    document.getElementById("hour").className = "hour hour-" + hours;
-}
+    var second_hand = document.getElementById("second");
+    var subsecond_hand = document.getElementById("subsecond");
 
-window.addEventListener('DOMContentLoaded', init);
+    function display(tick) {
+      second_hand.style.transform = "rotate(" + (360 * tick/3600 - 180) + "deg)";
+      subsecond_hand.style.transform = "rotate(" + (360 * tick/60 - 180) + "deg)";
+    }
+
+    var tick = 0;
+    var main = document.getElementsByTagName("main")[0];
+    main.addEventListener("click", () => {
+      tick++;
+      display(tick);
+    });
+
+    display(0);
+});
